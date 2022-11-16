28e1f19dfb6fe424bc6791d208b22f8b86d4e8c1
vladiuse <vladiuse@gmail.com>
2022-11-17 00:31:21 +0300

    join all styles in project

--- vitrina/vitrina/settings.py
+++ vitrina/vitrina/settings.py
@@ -125,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/home/v/vladiuse/vitrina/public_html/static'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
